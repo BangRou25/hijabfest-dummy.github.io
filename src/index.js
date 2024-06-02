b92b80beb0606bf040647491b62292636ce5d1ab
Rou Hamdani <140482896+BangRou25@users.noreply.github.com>
2024-06-02 18:58:45 +0700

    revision #1

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const modal = document.getElementById('contactModal');
+
+
+function showContact() {
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,22 @@ module.exports = {
       primary: "#ffd500",
       secondary: "#fafafa",
       tertiary: "#202020",
-      accent: "#",
+      rnd: "#3ac41e",
       dark: "#fafafa",
+      sponsor2: "#ce0026",
+      sponsor3: "#ffffff",
+      modalbg: "rgba(20,20,20,0.4)"
     },
     extend: {
       backgroundImage: {
-        "narsum-1": "url(./img/narsum-1.jpg)",
-        "narsum-2": "url(./img/narsum-2.jpg)"
+        "gueststar-1": "url(./img/gueststar-1.jpg)",
+        "gueststar-2": "url(./img/gueststar-2.jpg)",
+        "gueststar-3": "url(./img/gueststar-3.jpg)",
+        "sponsor-1": "url(./img/sponsor-1.png)",
+        "sponsor-2": "url(./img/sponsor-2.jpg)",
+        "sponsor-3": "url(./img/sponsor-3.jpg)",
+        "mc-1": "url(./img/mc-1.jpg)",
+        "mc-2": "url(./img/mc-2.jpg)"
       },
     },
   },
